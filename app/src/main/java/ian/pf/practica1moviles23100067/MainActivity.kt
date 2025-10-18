97a510e497953fc6d9e84d52eac5bb21b9e992d5
Ian Flores <23100067@ue.edu.pe>
2025-10-18 10:09:43 -0500

    feat: Implement navigation graph
    Adds the navigation graph to handle navigation between different screens of the application.
    
    - Creates the `AppNavGraph` composable, which defines the navigation routes.
    - Sets up a `NavHost` with the `HomeScreen` as the starting destination.
    - Defines composable routes for `home`, `calculatorWater`, `registerActivity`, and `catalog`.
    - Adds the `navigation-compose` dependency to `app/build.gradle.kts` and `gradle/libs.versions.toml` to enable Jetpack Navigation Compose.

--- app/src/main/java/ian/pf/practica1moviles23100067/MainActivity.kt
+++ app/src/main/java/ian/pf/practica1moviles23100067/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ian.pf.practica1moviles23100067.presentation.navigation.AppNavGraph
 import ian.pf.practica1moviles23100067.ui.theme.PRACTICA1MOVILES23100067Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PRACTICA1MOVILES23100067Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavGraph()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PRACTICA1MOVILES23100067Theme {
-        Greeting("Android")
     }
 }
