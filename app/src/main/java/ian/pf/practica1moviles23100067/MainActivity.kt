823ef075b5d2763d18c2b8380b37b8d1bc9ebf33
Ian Flores <23100067@ue.edu.pe>
2025-10-18 07:50:54 -0500

    Initial commit

---
+++ app/src/main/java/ian/pf/practica1moviles23100067/MainActivity.kt
@@ -0,0 +1,47 @@
+package ian.pf.practica1moviles23100067
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ian.pf.practica1moviles23100067.ui.theme.PRACTICA1MOVILES23100067Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PRACTICA1MOVILES23100067Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PRACTICA1MOVILES23100067Theme {
+        Greeting("Android")
+    }
+}
