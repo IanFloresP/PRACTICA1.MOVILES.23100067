70c84107bc45c7d6507e5b7fe287bf04bc7b4368
Ian Flores <23100067@ue.edu.pe>
2025-10-18 10:08:26 -0500

    feat: Implement navigation graph
    Adds the navigation graph to handle navigation between different screens of the application.
    
    - Creates the `AppNavGraph` composable, which defines the navigation routes.
    - Sets up a `NavHost` with the `HomeScreen` as the starting destination.
    - Defines composable routes for `home`, `calculatorWater`, `registerActivity`, and `catalog`.
    - Adds the `navigation-compose` dependency to `app/build.gradle.kts` and `gradle/libs.versions.toml` to enable Jetpack Navigation Compose.

--- app/src/main/java/ian/pf/practica1moviles23100067/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/ian/pf/practica1moviles23100067/presentation/navigation/AppNavGraph.kt
@@ -1,2 +1,21 @@
 package ian.pf.practica1moviles23100067.presentation.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ian.pf.practica1moviles23100067.presentation.calculatorWater.CalculatorWaterScreen
+import ian.pf.practica1moviles23100067.presentation.catalog.CatalogScreen
+import ian.pf.practica1moviles23100067.presentation.home.HomeScreen
+import ian.pf.practica1moviles23100067.presentation.registActivity.ActivityScreen
+
+@Composable
+fun AppNavGraph(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home"){
+        composable ("home"){ HomeScreen(navController) }
+        composable ("calculatorWater"){ CalculatorWaterScreen(navController) }
+        composable ("registerActivity"){ ActivityScreen(navController) }
+        composable ("catalog"){  CatalogScreen(navController) }
+    }
+}
